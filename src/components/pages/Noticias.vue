33ae65f86c022545a868c75ed47072db0df21e94
Jônata Marcelino <jonata.marcelino83@gmail.com>
2026-02-22 19:07:44 -0300

    Corrigida a paginação das notícias

--- src/components/pages/Noticias.vue
+++ src/components/pages/Noticias.vue
@@ -39,7 +39,7 @@ import Pagina from "@BASICS/Pagina";
 import Paginate from "vuejs-paginate";
 import Titulo from "@BASICS/Titulo";
 
-import NoticiasService from "../../scripts/services/NoticiaService";
+import { getNoticias } from "../../scripts/services/NoticiaService";
 
 import Utils from "../../scripts/utils/dataFormatada";
 import Data from "../../scripts/utils/Data";
@@ -65,7 +65,7 @@ export default {
       totalPages: 0,
       naoTemNoticias: false,
       tag: "",
-      page: "",
+      page: 1,
       perPage: 5,
       loading: true,
     };
@@ -84,53 +84,63 @@ export default {
       },
     ],
   },
-  mounted() {
-    this.page = this.$route.params.page ? parseInt(this.$route.params.page) : 1;
-    this.tag = this.$route.params.tag ? this.$route.params.tag : "";
-    this.noticias = this.loadNoticias(this.perPage, this.page, this.tag);
+  async mounted() {
+    this.page = this.$route.query.page ? parseInt(this.$route.query.page) : 1;
+    this.tag = this.$route.query.tag ? this.$route.query.tag : "";
+    this.noticias = await this.loadNoticias(this.perPage, this.page, this.tag);
   },
   beforeUpdate() {
-    this.page = this.$route.params.page ? parseInt(this.$route.params.page) : 1;
-    this.tag = this.$route.params.tag ? this.$route.params.tag : "";
+    this.page = this.$route.query.page ? parseInt(this.$route.query.page) : 1;
+    this.tag = this.$route.query.tag ? this.$route.query.tag : "";
   },
   methods: {
-    loadNoticias(perPage, page = 1, tag = "") {
+    async loadNoticias(perPage, page = 1, tag = "") {
       this.loading = true;
       const noticiasTratadas = [];
-      NoticiasService.getNoticias(perPage, page, tag).then(noticiasDados => {
-        noticiasDados.data.forEach(noticia => {
-          const tagsAtual = [];
-          const noticiaTratada = {
-            id: noticia.id,
-            titulo: noticia.title.rendered,
-            subtitulo: noticia.acf.subtitulo,
-            data_publicacao: Utils.dataFormatada(
-              noticia.acf.data_de_publicacao * 1000,
-            ),
-            data: Data.dataFormatada(noticia.acf.data_de_publicacao * 1000),
-            hora: Hora.dataFormatada(noticia.acf.data_de_publicacao * 1000),
-            url: noticia.slug,
-            resumo: noticia.acf.resumo,
-            cover: noticia.featured_media
-              ? noticia._embedded["wp:featuredmedia"][0].source_url
-              : "imagem_padrao.png",
-            tags: tagsAtual,
-          };
-          noticiasTratadas.push(noticiaTratada);
-        });
-        this.totalNoticias = parseInt(noticiasDados.headers["x-wp-total"]);
-        this.totalPages = parseInt(noticiasDados.headers["x-wp-totalpages"]);
-        if (noticiasTratadas.length === 0) {
-          this.naoTemNoticias = true;
-        }
-        this.loading = false;
+
+      const noticias = await getNoticias(perPage, page, tag);
+
+      Object.entries(noticias.data).forEach(v => {
+        const noticia = v[1];
+        const tagsAtual = [];
+        const noticiaTratada = {
+          id: noticia.id,
+          titulo: noticia.title.rendered,
+          subtitulo: noticia.acf.subtitulo,
+          data_publicacao: Utils.dataFormatada(
+            noticia.acf.data_de_publicacao * 1000,
+          ),
+          data: Data.dataFormatada(noticia.acf.data_de_publicacao * 1000),
+          hora: Hora.dataFormatada(noticia.acf.data_de_publicacao * 1000),
+          url: noticia.slug,
+          resumo: noticia.acf.resumo,
+          cover: noticia.featured_media
+            ? noticia._embedded["wp:featuredmedia"][0].source_url
+            : "imagem_padrao.png",
+          tags: tagsAtual,
+        };
+        noticiasTratadas.push(noticiaTratada);
       });
+
+      this.totalNoticias = parseInt(noticias.headers["x-wp-total"]);
+      this.totalPages = parseInt(noticias.headers["x-wp-totalpages"]);
+      this.page = page;
+
+      if (noticiasTratadas.length === 0) {
+        this.naoTemNoticias = true;
+      }
+      this.loading = false;
+
       return noticiasTratadas;
     },
-    paginate(pageNum) {
+    async paginate(pageNum) {
       this.page = pageNum;
-      this.$router.push({ params: { page: pageNum } });
-      this.noticias = this.loadNoticias(this.perPage, this.page, this.tag);
+      this.$router.push({ query: { page: pageNum } });
+      this.noticias = await this.loadNoticias(
+        this.perPage,
+        this.page,
+        this.tag,
+      );
     },
   },
 };
--- src/scripts/services/NoticiaService.js
+++ src/scripts/services/NoticiaService.js
@@ -1,19 +1,20 @@
 import axios from "axios";
 import * as urls from "../config";
 
+export function getNoticias(perPage, page = 1, tags = "") {
+  if (tags !== "") {
+    tags = `&tags=${tags}`;
+  }
+  const service = `${urls.WP_URL}noticias-publicadas/?per_page=${perPage}&page=${page}&_embed${tags}`;
+  return axios
+    .get(service)
+    .then(response => response)
+    .catch(error => {
+      // console.log(error);
+    });
+}
+
 export default {
-  getNoticias(perPage, page = 1, tags = "") {
-    if (tags !== "") {
-      tags = `&tags=${tags}`;
-    }
-    const service = `${urls.WP_URL}noticias-publicadas/?per_page=${perPage}&page=${page}&_embed${tags}`;
-    return axios
-      .get(service)
-      .then(response => response)
-      .catch(error => {
-        // console.log(error);
-      });
-  },
   getNoticia(id) {
     const service = `${urls.WP_URL}noticias/${id}?_embed`;
     return axios
