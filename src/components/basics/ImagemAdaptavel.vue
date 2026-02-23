d86e8233c87b3aff8226938609f6e48bdd1b6070
Jônata Marcelino <jonata.marcelino83@gmail.com>
2026-02-22 23:59:41 -0300

    Ajustes na imagem da notícia

--- src/components/basics/ImagemAdaptavel.vue
+++ src/components/basics/ImagemAdaptavel.vue
@@ -6,91 +6,84 @@
 </template>
 
 <script>
-  import Lightbox from 'vue-simple-lightbox';
+import Lightbox from "vue-simple-lightbox";
 
-  export default {
-    components: {
-      Lightbox
-    },
-    props: ['imagem', 'legenda', 'altura', 'largura', 'isFullWidth'],
-    data() {
-        return {
-          images: [
-            {
-                src: this.imagem,
-                title: this.legenda
-            }
-          ],
-          options: {
-            closeText: 'X',
-            widthRatio: 1,
-            heightRatio: 1
-          }
-        };
-    },
-    computed: {
-      figureClasses() {
-        let classes = "";
-        if (this.isFullWidth) {
-          classes += "full-width";
-        }
-
-        return classes;
+export default {
+  components: {
+    Lightbox,
+  },
+  props: ["imagem", "legenda", "altura", "largura", "isFullWidth"],
+  data() {
+    return {
+      images: [
+        {
+          src: this.imagem,
+          title: this.legenda,
+        },
+      ],
+      options: {
+        closeText: "X",
+        widthRatio: 1,
+        heightRatio: 1,
+      },
+    };
+  },
+  computed: {
+    figureClasses() {
+      let classes = "";
+      if (this.isFullWidth) {
+        classes += "full-width";
       }
-    }
-  };
+
+      return classes;
+    },
+  },
+};
 </script>
 
 <style lang="scss">
+div#figure figure {
+  max-width: 35%;
+  float: left;
+  margin-right: 20px;
+  margin-top: 15px;
+}
 
-  .my-gallery a img {
-    width: 100%;
-  }
+div#figure figcaption {
+  text-align: center;
+}
 
+@include media("<tablet") {
   div#figure figure {
-    max-width: 35%;
-    float: left;
-    margin-right: 20px;
-    margin-top: 15px;
-  }
-
-  div#figure figcaption {
-    text-align: center;
-  }
-
-  @include media("<tablet") {
-
-    div#figure figure {
-      float: none;
-      max-width: 100%;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      margin-right: 0;
-    }
-
-    .my-gallery a img {
-      max-height: 180px;
-      max-width: 100%;
-      object-fit: cover;
-    }
-
-  }
-
-  .figure.full-width{
+    float: none;
+    max-width: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
-    width: 100%;
-    padding: 20px;
+    margin-right: 0;
   }
 
-  .figure.full-width figure {
-    min-width: 50%;
-    max-width: 70%;
-    margin: auto;
-    float: none;
-    margin-right: auto !important;
-    margin-top: 15;
+  .my-gallery a img {
+    max-height: 180px;
+    max-width: 100%;
+    object-fit: cover;
   }
+}
+
+.figure.full-width {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 20px;
+}
+
+.figure.full-width figure {
+  min-width: 50%;
+  max-width: 70%;
+  margin: auto;
+  float: none;
+  margin-right: auto !important;
+  margin-top: 15;
+}
 </style>
